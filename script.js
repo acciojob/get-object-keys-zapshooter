69645b45f3305f62b20d5a08032e3238836cde32
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-04-02 23:19:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Create the student object
+const student = {
+  name: "John"
+};
+
+// Function to get all keys from the object
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+// Test
+console.log(getKeys(student));
